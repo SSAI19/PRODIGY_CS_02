28c888fb24697df7c9dc18df832281e21a520e8b
SSAI19 <saiarchanaa2005@gmail.com>
2024-11-03 15:49:04 +0530

    Add files via upload

---
+++ Task 02.py
@@ -0,0 +1,36 @@
+from PIL import Image
+
+def encrypt_image(input_path, output_path, key):
+    img = Image.open(input_path)
+    pixels = img.load()
+    width, height = img.size
+    for i in range(width):
+        for j in range(height):
+            r, g, b = pixels[i, j]
+            encrypted_pixel = (b, g, r)
+            pixels[i, j] = encrypted_pixel
+    img.save(output_path)
+    print("Image encrypted successfully!")
+
+def decrypt_image(input_path, output_path, key):
+    img = Image.open(input_path)
+    pixels = img.load()
+    width, height = img.size
+    for i in range(width):
+        for j in range(height):
+            r, g, b = pixels[i, j]
+            decrypted_pixel = (b, g, r)
+            pixels[i, j] = decrypted_pixel
+    img.save(output_path)
+    print("Image decrypted successfully!")
+
+# Image paths (replace 'S1.jpg' with your uploaded image filename)
+input_image = r "C:\Users\HP\OneDrive\Desktop\Internship\S1.jpg"
+encrypted_image = r "C:\Users\HP\OneDrive\Desktop\Internship\encrypted_image.jpg"
+decrypted_image = r "C:\Users\HP\OneDrive\Desktop\Internship\decrypted_image.jpg"
+
+# Encrypt the image
+encrypt_image(input_image, encrypted_image, key=None)
+
+# Decrypt the image
+decrypt_image(encrypted_image, decrypted_image, key=None)
